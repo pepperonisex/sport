611190419fa450494584fba4a3200115aaa3f68b
pepperonnisex <tonio22590@gmail.com>
2024-08-05 14:29:47 +0200

    fix range + couleurs avant opti

--- test.py
+++ test.py
@@ -22,9 +22,9 @@ client = gspread.authorize(creds)
 spreadsheet = client.open_by_key('16QakQHtFK7TTm5c8-bDBHqSul7BYRNzERqPy6xpYK7M')
 worksheet = spreadsheet.worksheet('test_api')
 
-spawn_col = "A"
-spawn_row = 1
-current_date = datetime.now().strftime("%Y-%m-%d")  # Format date as needed
+spawn_col = "C"
+spawn_row = 10
+current_date = datetime.now().strftime("%Y-%m-%d")
 program_name = "Programme 1"
 workout_data = {
     'pompes': [15, 16, 16, 12],
@@ -45,34 +45,38 @@ end_row_index = start_row_index + max_length
 all_range = {
     "sheetId": worksheet._properties['sheetId'],
     "startRowIndex": start_row_index,
-    "endRowIndex": end_row_index,
+    "endRowIndex": end_row_index + 1,
     "startColumnIndex": start_col_index - 1,
-    "endColumnIndex": end_col_index
+    "endColumnIndex": end_col_index + 1
 }
+all_range_color = {"red": 56.0, "green": 90.0, "blue": 10.0, "alpha": 1}
 
 date_range = {
     "sheetId": worksheet._properties['sheetId'],
     "startRowIndex": start_row_index,
-    "endRowIndex": start_row_index + 1,
+    "endRowIndex": end_row_index + 1,
     "startColumnIndex": start_col_index - 1,
     "endColumnIndex": start_col_index
 }
+date_range_color = {"red": 56.0, "green": 90.0, "blue": 90.0, "alpha": 1}
 
 cols_headers_range = {
     "sheetId": worksheet._properties['sheetId'],
     "startRowIndex": start_row_index,
     "endRowIndex": start_row_index + 1,
-    "startColumnIndex": start_col_index,
-    "endColumnIndex": end_col_index
+    "startColumnIndex": start_col_index + 1,
+    "endColumnIndex": end_col_index + 1
 }
+cols_headers_range_color = {"red": 56.0, "green": 90.0, "blue": 10.0, "alpha": 1}
 
 rows_headers_range = {
     "sheetId": worksheet._properties['sheetId'],
     "startRowIndex": start_row_index + 1,
-    "endRowIndex": end_row_index,
-    "startColumnIndex": start_col_index - 1,
-    "endColumnIndex": start_col_index
+    "endRowIndex": end_row_index + 1,
+    "startColumnIndex": start_col_index,
+    "endColumnIndex": start_col_index + 1
 }
+rows_headers_range_color = {"red": 5.0, "green": 70.0, "blue": 80.0, "alpha": 1}
 
 program_name_range = {
     "sheetId": worksheet._properties['sheetId'],
@@ -81,64 +85,60 @@ program_name_range = {
     "startColumnIndex": start_col_index,
     "endColumnIndex": start_col_index + 1
 }
+program_name_range_color = {"red": 56.0, "green": 10.0, "blue": 48.0, "alpha": 1}
 
 table_range = {
+    'sheetId': worksheet._properties['sheetId'],
+    'startRowIndex': spawn_row - 1,
+    'endRowIndex': end_row_index + 1,
+    'startColumnIndex': start_col_index,
+    'endColumnIndex': end_col_index + 1
+}
+program_name_range_color = {"red": 40.0, "green": 90.0, "blue": 62.0, "alpha": 1}
+
+table_value_range = {
     "sheetId": worksheet._properties['sheetId'],
     "startRowIndex": start_row_index + 1,
-    "endRowIndex": end_row_index,
-    "startColumnIndex": start_col_index,
-    "endColumnIndex": end_col_index
+    "endRowIndex": end_row_index + 1,
+    "startColumnIndex": start_col_index + 1,
+    "endColumnIndex": end_col_index + 1
 }
+table_value_range_color = {"red": 90.0, "green": 10.0, "blue": 10.0, "alpha": 1}
+
+ranges_and_colors = [
+    (all_range, all_range_color),
+    (date_range, date_range_color),
+    (cols_headers_range, cols_headers_range_color),
+    (rows_headers_range, rows_headers_range_color),
+    (program_name_range, program_name_range_color),
+    (table_value_range, table_value_range_color)
+]
 
 
-requests = [
+static_requests = [
     {
         "mergeCells": {
             "mergeType": "MERGE_COLUMNS",
-            "range": {
-                "sheetId": worksheet._properties['sheetId'],
-                "startRowIndex": spawn_row - 1,
-                "endRowIndex": spawn_row + len(data) -1,
-                "startColumnIndex": start_col_index - 1,
-                "endColumnIndex": start_col_index
-            }
+            "range": date_range
         }
     },
     {
         'updateCells': {
             'rows': [{'values': [{'userEnteredValue': {'stringValue': str(cell)}} for cell in row]} for row in data],
             'fields': 'userEnteredValue',
-            'range': {
-                'sheetId': worksheet._properties['sheetId'],
-                'startRowIndex': spawn_row - 1,
-                'endRowIndex': spawn_row + len(data) - 1,
-                'startColumnIndex': start_col_index,
-                'endColumnIndex': end_col_index + 1
-            }
+            'range': table_range
         },
     },
     {
         'updateCells': {
             'rows': [{'values': [{'userEnteredValue': {'stringValue': current_date}}]}],
             'fields': 'userEnteredValue',
-            'range': {
-                'sheetId': worksheet._properties['sheetId'],
-                'startRowIndex': spawn_row - 1,
-                'startColumnIndex': start_col_index - 1,
-                'endRowIndex': spawn_row + 1,
-                'endColumnIndex': start_col_index
-            }
+            'range': date_range
         }
     },
     {
         "repeatCell": {
-            "range": {
-                "sheetId": worksheet._properties['sheetId'],
-                "startRowIndex": spawn_row - 1,
-                "endRowIndex": spawn_row,
-                "startColumnIndex": start_col_index - 1,
-                "endColumnIndex": start_col_index
-            },
+            "range": date_range,
             "cell": {
                 "userEnteredFormat": {
                     "horizontalAlignment": "CENTER",
@@ -150,13 +150,7 @@ requests = [
     },
     {
         "repeatCell": {
-            "range": {
-                "sheetId": worksheet._properties['sheetId'],
-                "startRowIndex": spawn_row - 1,
-                "endRowIndex": spawn_row + len(data) - 1,
-                "startColumnIndex": start_col_index,
-                "endColumnIndex": end_col_index + 1
-            },
+            "range": table_range,
             "cell": {
                 "userEnteredFormat": {
                     "horizontalAlignment": "CENTER"
@@ -167,6 +161,23 @@ requests = [
     }
 ]
 
+color_requests = [
+    {
+        "repeatCell": {
+            "range": range_info,
+            "cell": {
+                "userEnteredFormat": {
+                    "backgroundColor": color_info
+                }
+            },
+            "fields": "userEnteredFormat.backgroundColor"
+        }
+    } for range_info, color_info in ranges_and_colors
+]
+
+requests = static_requests + color_requests
+
 formatted_json = json.dumps(requests, indent=4)
 print(formatted_json)
+
 spreadsheet.batch_update({'requests': requests})
